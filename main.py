27bc5e6fd8526210d2b2f64ec16034f2aec2e362
Astri_ <malo1.thiebaud@gmail.com>
2022-10-14 19:09:22 +0200

    Remove whitespace matching
    Allow the user to put extra whitespace characters (space, linebreaks, etc) to match kattis rules

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 import zipfile
 import io
-from pathlib import Path
+import re
 
 import requests
 
@@ -17,16 +17,17 @@ with zipfile.ZipFile(io.BytesIO(r.content)) as z:
     for i in range(1,len(z.filelist)//2+1):
         if(len(test_to_run) == 0 or str(i) in test_to_run):
             test_input = z.read(f"{i}.in").decode("utf-8")
-            test_answer = z.read(f"{i}.ans").decode("utf-8")
+            test_answer = re.sub(r"\s+$","",z.read(f"{i}.ans").decode("utf-8"))
             p = subprocess.run([sys.executable, sys.argv[2]],
                                 input=test_input, encoding="utf-8", capture_output=True)
             if p.returncode != 0:
                 print("Error:", p.stderr)
+            out = re.sub(r"\s+$","",p.stdout)
             print("Sample input:\n", test_input, sep="")
-            if p.stdout != test_answer:
+            if out != test_answer:
                 print("Wrong answer!")
                 print("Expected:", len(test_answer),"\n", test_answer, sep="")
-                print("Got:", len(p.stdout),"\n", p.stdout, sep="")
+                print("Got:", len(out),"\n", out, sep="")
             else:
                 print("Good answer!")
-                print("Answer:\n", p.stdout, sep="")
+                print("Answer:\n", out, sep="")
